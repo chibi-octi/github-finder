e9289413d27bf65829e7f63178b9e91766de0921
Declan Talbert <declan_at@hotmail.com>
2020-02-22 22:47:34 +0000

    Connected GitHub API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React, { Component } from 'react';
-import './App.css';
 import Navbar from './components/layout/Navbar';
-import UserItem from './components/users/UserItem';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import axios from 'axios';
+import './App.css';
 
 class App extends Component {
-  foo = () => 'Bars';
+  state = {
+    users: [],
+    loading: false
+  }
+
+  async componentDidMount() {
+    this.setState({loading: true});
+
+    const res = await axios.get(`https://api.github.com/users?client_id=
+    ${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=
+    ${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+
+    this.setState({users: res.data, loading: false});
+  }
+
 
   render() {
 
     return (
       <div className="App">
         <Navbar /> 
-        <UserItem />
+        <div className="container">
+          <Search />
+          <Users loading={this.state.loading} users={this.state.users} />
+        </div>
+        
       </div>
     );
   }
