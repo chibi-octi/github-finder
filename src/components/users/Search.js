9d5c89dccbace2317fe63cd75f86babdc1405bce
Declan Talbert <declan@Declans-MacBook-Pro.local>
2020-07-02 15:37:11 +0100

    search working and clear for Search.js

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,19 +1,29 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 export class Search extends Component {
     state = {
         text: ''
-    }
+    };
 
+    static propTypes = {
+        searchUsers: PropTypes.func.isRequired,
+        clearhUsers: PropTypes.func.isRequired,
+        showClear: PropTypes.bool.isRequired
+    };
+ 
     onSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state.text);
+        this.props.searchUsers(this.state.text);
+        this.setState({ text: ''});
     };
 
     onChange = (e) => this.setState({ [e.target.name]: e.target.value });
 
 
     render() {
+        const { showClear, clearUsers } = this.props;
+
         return (
             <div>
                 <form onSubmit={this.onSubmit} className="form">
@@ -22,6 +32,14 @@ export class Search extends Component {
                     onChange={this.onChange}/>
                     <input type="submit" value="search" className="btn btn-dark btn-block"/>
                 </form>
+                {showClear && (
+                    <button 
+                        classeName="btn btn-light btn-block" 
+                        onClick={clearUsers}>
+                        Clear
+                    </button>
+                )}
+                
             </div>
         )
     }
