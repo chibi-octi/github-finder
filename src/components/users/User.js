79962db976d9f641280d07e69741224b6df5506c
Declan Talbert <declan_at@hotmail.com>
2020-10-12 09:54:31 +0100

    User.js development with data

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -21,6 +21,7 @@ export class User extends Component {
             location,
             bio,
             blog,
+            company,
             login,
             html_url,
             followers,
@@ -39,8 +40,56 @@ export class User extends Component {
                 <Link to='/' className='btn btn-light'>
                     Back to Search
                 </Link>
+                Hireable: {' '}
+                {hireable ? <i className="fas fa-check text-success"/> : <i className="fas fa-times-circle text-danger" />}
+                <div className="card grid-2">
+                    <div className="all-center">
+                        <img src={avatar_url} className="round-img" alt="" style={{width:'150px'}}/>
+                        <h1>{name}</h1>
+                        <p>{location}</p>
+                    </div>
+                    <div>
+                        {bio && (
+                            <Fragment>
+                                <h3>bio</h3>
+                                <p>{bio}</p>
+                            </Fragment>
+                        )}
+                        <a href={html_url} className="btn btn-dark my-1">Visit GitHub Profile</a>
+                        <ul>
+                            <li>
+                                {login && 
+                                    <Fragment>
+                                        <strong>Username: </strong> {login}
+                                    </Fragment>
+                                }
+                            </li>
+                            <li>
+                                {company && 
+                                    <Fragment>
+                                        <strong>Company: </strong> {company}
+                                    </Fragment>
+                                }
+                            </li>
+                            <li>
+                                {blog && 
+                                    <Fragment>
+                                        <strong>Website: </strong> {blog}
+                                    </Fragment>
+                                }
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+
+                <div className="card text-center">
+                    <div className="badge badge-primary">Followers: {followers}</div>
+                    <div className="badge badge-success">Following: {following}</div>
+                    <div className="badge badge-light">Public Repos: {public_repos}</div>
+                    <div className="badge badge-dark">Public Gists: {public_gists}</div>
+                </div>
             </Fragment>
-        )
+        );
     }
 
 }
