c9060d5e27098c5bfa7f4750a40123b6719a4029
Declan Talbert <declan_at@hotmail.com>
2020-10-15 10:56:43 +0100

    32. Moving Repos State To Context - Completed

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -24,8 +24,14 @@ export default (state, action) => {
             return {
                 ...state,
                 users: [],
-                laoding: false
+                loading: false
             };
+        case GET_REPOS:
+            return {
+                ...state,
+                repos: action.payload,
+                loading: false
+            }
         case SET_LOADING:
             return {
                 ...state,
